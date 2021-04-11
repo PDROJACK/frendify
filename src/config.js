0f3324dc519ed515654d3b8a88bb28d04e7d28a0
pdrojack <singhpd75@gmail.com>
2021-04-11 18:13:45 +0530

    changed config again

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ const config ={
         'SPOTIFY': 'https://api.spotify.com/v1'
     }, 
     "development": {
-        'BACKEND': 'http://localhost:4000/',
-        'SOCKET': 'http://localhost:8080',
+        'BACKEND': 'https://spotichat-b.herokuapp.com/',
+        'SOCKET': 'https://spotichat-b.herokuapp.com:8080',
         'SPOTIFY': 'https://api.spotify.com/v1'
     }
 }
