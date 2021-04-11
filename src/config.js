932f1a4b8de3addee8e5b84581e6f610c3a35794
pdrojack <singhpd75@gmail.com>
2021-04-11 19:19:31 +0530

    Config change dont judge this

--- src/config.js
+++ src/config.js
@@ -5,8 +5,8 @@ const config ={
         'SPOTIFY': 'https://api.spotify.com/v1'
     }, 
     "development": {
-        'BACKEND': 'http://localhost:4000/',
-        'SOCKET': 'http://localhost:8080',
+        'BACKEND': 'https://frendify-b.herokuapp.com/',
+        'SOCKET': 'https://frendify-b.herokuapp.com:8080',
         'SPOTIFY': 'https://api.spotify.com/v1'
     }
 }
