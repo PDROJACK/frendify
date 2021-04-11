f565e333e73b6271a2d2b2551b2b684217e59bc9
pdrojack <singhpd75@gmail.com>
2021-04-11 19:15:17 +0530

    Config commit

--- src/config.js
+++ src/config.js
@@ -1,12 +1,12 @@
 const config ={
     "production": {
-        'BACKEND': 'https://spotichat-b.herokuapp.com/',
-        'SOCKET': 'https://spotichat-b.herokuapp.com:8080',
+        'BACKEND': 'https://frendify-b.herokuapp.com/',
+        'SOCKET': 'https://frendify-b.herokuapp.com:8080',
         'SPOTIFY': 'https://api.spotify.com/v1'
     }, 
     "development": {
-        'BACKEND': 'https://spotichat-b.herokuapp.com/',
-        'SOCKET': 'https://spotichat-b.herokuapp.com:8080',
+        'BACKEND': 'http://localhost:4000/',
+        'SOCKET': 'http://localhost:8080',
         'SPOTIFY': 'https://api.spotify.com/v1'
     }
 }
